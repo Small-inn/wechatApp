74f6844632c6b8a50e135853bffec80a4f1efc6c
Small_inn <small_inn@163.com>
2018-11-30 17:52:22 +0800

    添加背景音乐，尚未完成

--- pages/post-detail/post-detail.js
+++ pages/post-detail/post-detail.js
@@ -1,4 +1,5 @@
 import postData from '../../data/data.js'
+const backgroundAudioManager = wx.getBackgroundAudioManager() 
 Page({
 
   /**
@@ -6,16 +7,17 @@ Page({
    */
   data: {
     currentPostId: '',
-    isPlay: false
+    isPlay: false, // 音乐播放控制
+    // backgroundAudioManager: null // 背景音频对象
   },
-
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
     const id = options.id
     // 利用data缓存id
-    this.currentPostId = id
+    this.data.currentPostId = id
+    // 获取数据
     this.setData({ postDetail: postData[id] })
     // 获取缓存值
     var postCollectedObj = wx.getStorageSync('post_collected')
@@ -27,6 +29,14 @@ Page({
       postCollectedObj[id] = false
       wx.setStorageSync('post_collected', postCollectedObj)
     }
+    // 监听音乐播放与暂停
+    // this.setData({ backgroundAudioManager: wx.getBackgroundAudioManager() })
+    // this.data.backgroundAudioManager.onPause(() => {
+    //   console.log('暂停')
+    // })
+    // this.data.backgroundAudioManager.onPlay(() => {
+    //   console.log('播放')
+    // })
   },
   // 收藏实现
   onCollect: function (event) {
@@ -53,17 +63,24 @@ Page({
 
   // 音乐播放实现
   onMusicTap: function (e) {
-    console.log(e)
-    if (this.isPlay) {
-      wx.pauseBackgroundAudio()
-      this.isPlay = false
+    console.log(postData)
+    const currentId = this.data.currentPostId
+    console.log(backgroundAudioManager)
+    // this.data.backgroundAudioManager.title = postData[currentId].musicTitle
+    backgroundAudioManager.title = '此时此刻'
+    backgroundAudioManager.epname = '此时此刻'
+    backgroundAudioManager.singer = '许巍'
+    backgroundAudioManager.coverImgUrl = 'http://y.gtimg.cn/music/photo_new/T002R300x300M000003rsKF44GyaSk.jpg?max_age=2592000'
+    backgroundAudioManager.src = 'http://ws.stream.qqmusic.qq.com/M500001VfvsJ21xFqb.mp3?guid=ffffffff82def4af4b12b3cd9337d5e7&uin=346897220&vkey=6292F51E1E384E061FF02C31F716658E5C81F5594D561F2E88B854E81CAAB7806D5E4F103E55D33C16F3FAC506D1AB172DE8600B37E43FAD&fromtag=46'
+    // backgroundAudioManager.src = postData[currentId].musicUrl
+    if (this.data.isPlay) {
+      // backgroundAudioManager.pause()
+      this.setData({ isPlay: false })
+      // this.data.isPlay = false
     } else {
-      this.isPlay = true
-      wx.playBackgroundAudio({
-        dataUrl: postData[this.currentPostId].musicUrl,
-        title: postData[this.currentPostId].musicTitle,
-        coverImgUrl: postData[this.currentPostId].coverImg
-      })
+      backgroundAudioManager.play()
+      this.setData({ isPlay: true })
+      // this.data.isPlay = true
     }
   }
 })
--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -8,7 +8,12 @@ Page({
   onClickTap(e) {
     wx.navigateTo({
       url: '../posts/post',
-    })
+    });
+    // 不存在子父页面的关系，到下一个页面之前的页面会被销毁，执行前一个页面的Unload事件
+    // wx.redirectTo({
+    //   url: '',
+    // })
+
   },
   onGotUserInfo(e) {
     console.log(e.detail.userInfo)
